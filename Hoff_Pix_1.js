82fcad6a0d6be2f1a511eea3fe26c4e3d9567624
Daniil <67416922+da9iilzzz@users.noreply.github.com>
2025-03-20 19:11:47 +0300

    Add files via upload

---
+++ Hoff_Pix_1.js
@@ -0,0 +1,25 @@
+<script type="text/javascript">
+    if (typeof __GetI === "undefined") {
+        __GetI = [];
+    }
+    (function () {
+        var p = {
+            type: "VIEW",
+            /* config START */
+            site_id: "10176",
+            product_id: "",
+            product_price: "",
+            category_id: "",
+            pixel_id: ""
+            /* config END */
+        };
+        __GetI.push(p);
+        var domain = (typeof __GetI_domain) == "undefined" ? "px.adhigh.net" : __GetI_domain;
+        var src = ('https:' == document.location.protocol ? 'https://' : 'http://') + domain + '/t.js';
+        var script = document.createElement( 'script' );
+        script.type = 'text/javascript';
+        script.src = src;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
+    })();
+</script>
